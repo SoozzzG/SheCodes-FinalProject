6ba4317a4348329e68a6131eba37dbddfb2181b4
Sooz Green <80789024+SoozzzG@users.noreply.github.com>
2021-03-17 11:26:40 -0600

    Moved final project to github folder

---
+++ index.js
@@ -0,0 +1,78 @@
+let now = new Date();
+let date = now.getDate();
+let year = now.getFullYear();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+
+let currentDate = document.querySelector("#current-date-time");
+currentDate.innerHTML = `${day} <br /> ${month} ${date}, ${year} <br/> ${hours}:${minutes}`;
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-search").value;
+  searchCity(city);
+}
+
+function searchCity(city) {
+  let apiKey = "5c7a71a4ec2cbbdeb5946ac58e7b7013";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function showTemperature(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function showPosition(position) {
+  let apiKey = "5c7a71a4ec2cbbdeb5946ac58e7b7013";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function getCurrentLocation(event) {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let searchLocationForm = document.querySelector("#new-location");
+searchLocationForm.addEventListener("submit", handleSubmit);
+
+let currentButton = document.querySelector("#current-location-button");
+currentButton.addEventListener("click", getCurrentLocation);
+
+searchCity("New York");
